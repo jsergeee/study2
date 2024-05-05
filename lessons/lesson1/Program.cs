af3948b49d6b6bf9571920fb0e3255aa67d72348
sergey yakovenko <sergey.w.yakovenko@gmail.com>
2024-05-05 16:05:44 +0500

    lesson1

---
+++ lessons/lesson1/Program.cs
@@ -0,0 +1,36 @@
+﻿double d = 5.5;
+char c = 'i';
+bool b = true;
+Console.WriteLine(b);
+Console.WriteLine(c);
+Console.WriteLine(d);
+
+int i1 = 5;
+int i2 = 6;
+int sum = i1 + i2;
+Console.WriteLine(sum);
+
+long l1 = 6;
+long sum2 = l1 + i1;
+Console.WriteLine(sum2);
+
+double d1 = 5.5;
+double sum3 = d1 + i1;
+
+Console.WriteLine(sum3);
+
+int product1 = i1 * i2;
+long product2 = l1 * i1;
+double product3 = i1 * d1;
+Console.WriteLine(product1);
+Console.WriteLine(product2);
+Console.WriteLine(product3);
+
+int quantient = i1 / i2;
+
+Console.WriteLine(quantient);
+
+d1 = 6;
+double quantient2 = i1 / d1;
+
+Console.WriteLine(quantient2);
